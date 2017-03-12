961bc43111ec2ace6ccd71acc34ca696bbbd073d
Kevin <kevingm31@gmail.com>
2017-03-12 11:59:35 -0500

    Add links to clean up app

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,8 +1,55 @@
 class ProductsController < ApplicationController
-  def bat_all_method
-    @bats = Product.all
-    render "bat_one.html.erb"
+  def index
+    @products = Product.all
+    render "index.html.erb"
   end
 
-  
+  def new
+    render "new_bat.html.erb"
+  end
+
+  def create
+    product = Product.new(
+      name: params[:name],
+      color: params[:color],
+      disposition: params[:disposition],
+      image: params[:image],
+      price: params[:price]
+    )
+    product.save
+    render "create.html.erb"
+  end
+
+  def show
+    product_id = params[:id]
+    @product = Product.find_by(id: product_id)
+    render "show.html.erb"
+  end
+
+  def edit
+    product_id = params[:id]
+    @product = Product.find_by(id: product_id)
+    render "edit.html.erb"
+  end
+
+  def update
+    product_id = params[:id]
+    @product = Product.find_by(id: product_id)
+    @product.name = params[:name]
+    @product.color = params[:color]
+    @product.disposition = params[:disposition]
+    @product.image = params[:image]
+    @product.price = params[:price]
+    @product.save
+    render "update.html.erb"
+  end
+
+  def destroy
+    product_id = params[:id]
+    @product = Product.find_by(id: product_id)
+    @product.destroy
+    render "destroy.html.erb"
+  end
+
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,12 @@
 Rails.application.routes.draw do
-  get "/bat_all_url" => "products#bat_all_method"
+  get "/products" => "products#index"
+  get "/products/new" => "products#new"
+  post "/products" => "products#create"
+  get "/product/:id" => "products#show"
+  get "/products/:id/edit" => "products#edit"
+  patch "/products/:id" => "products#update"
+  delete "/products/:id" => "products#destroy"
+
+
   
 end
